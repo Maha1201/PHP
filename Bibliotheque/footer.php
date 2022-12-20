191075934a25d992637d76069857402782fd0ec8
Maha1201 <abdalla.m1201@gmail.com>
2022-12-20 16:31:37 +0100

    Suite

--- Bibliotheque/footer.php
+++ Bibliotheque/footer.php
@@ -5,7 +5,7 @@
                 <li class="nav-item"><a href="#" class="nav-link">plandusite</a><li>
             </ul>
         </footer>
-    </div> <!-- fermerure de la div class="container" -->
+    </div> <!-- fermeture de la div class="container" -->
 
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
         integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous">
